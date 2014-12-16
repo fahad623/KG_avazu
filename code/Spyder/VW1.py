254d2e6e239b5bcb7b70c6b1137f8335784f0e19
fahad <fahad2191@gmail.com>
2014-12-16 17:23:07 -0600

    a

--- code/Spyder/VW1.py
+++ code/Spyder/VW1.py
@@ -1,13 +1,6 @@
-vw avazu.train.vw -c -k --passes 300 -b 24 -f avazu.model.vw --loss_function logistic --l2 0.000001
+L2 REgularization -
+vw avazu.train.vw -c --passes 300 -b 24 -f avazu.model.vw --loss_function logistic --l2 0.000001
 vw avazu.test.vw -t -i avazu.model.vw -p avazu.preds.txt --link logistic
-
-vw avazu.train.vw -c -k --passes 9 -b 24 -f avazu.model.vw --holdout_off --loss_function logistic --l2 1e-9
-
-vw avazu.train.vw -c -k --passes 300 -b 24 -f avazu.model.vw --loss_function hinge
-vw avazu.test.vw -t -i avazu.model.vw -p avazu.preds.txt --link logistic
-
-
-
 
 None       0.3838 8
 
@@ -16,7 +9,32 @@ None       0.3838 8
 1e-9       0.3837 9
 1e-10      0.3838 8
 
+L1 REgularization -
+vw avazu.train.vw -c --passes 300 -b 24 -f avazu.model.vw --loss_function logistic --l1 1e-9
+vw avazu.test.vw -t -i avazu.model.vw -p avazu.preds.txt --link logistic
+
+
+None        
+
+1e-7     0.3983    4    3048156303
+1e-8     0.3901    5
+1e-9     0.3841    9    6858345753
 
 
 
-~/code/vowpal_wabbit/utl/vw-varinfo --passes 1 -b 24 --readable_model avazu.model.vw --loss_function logistic avazu.trunc_train.vw
+Smaller file -
+vw avazu.trunc_train.vw -c --passes 300 -b 24 -f avazu.model.vw --loss_function logistic
+
+Holdout off - 
+vw avazu.train.vw -c -k --passes 9 -b 24 -f avazu.model.vw --holdout_off --loss_function logistic --l2 1e-9
+
+HInge - 
+vw avazu.train.vw -c -k --passes 300 -b 24 -f avazu.model.vw --loss_function hinge
+vw avazu.test.vw -t -i avazu.model.vw -p avazu.preds.txt --link logistic
+
+
+
+Feature names - 
+~/code/vowpal_wabbit/utl/vw-varinfo -c --passes 3 -b 24 -f avazu.model.vw --loss_function logistic avazu.train.vw > varinfo.txt
+
+vw avazu.trunc_train.vw --passes 1 -b 24 --invert_hash avazu.model.vw --loss_function logistic 
