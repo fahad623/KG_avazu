54057c610023625d6829be30b62ced5ffeda637b
fahad <fahad2191@gmail.com>
2015-02-08 21:56:47 -0600

    a

--- code/Spyder/VW1.py
+++ code/Spyder/VW1.py
@@ -54,7 +54,33 @@ Feature names -
 vw avazu.trunc_train.vw --passes 1 -b 24 --invert_hash avazu.model.vw --loss_function logistic 
 
 
+Hypersearch - 
 
+~/code/vowpal_wabbit/utl/vw-hypersearch -L 1e-10 1e-6 vw avazu.train.vw -c --passes 300 -b 24 -f l1_model.vw --loss_function logistic --l1 %
 
 clicks:  6865066
 no_clicks:  33563901
+
+
+
+train - 
+vw avazu.train.vw -c --passes 300 -b 24 -f avazu.model.vw --loss_function logistic
+vw avazu.train.vw -c --passes 8 -b 24 -f avazu.model.vw --holdout_off --loss_function logistic
+test -
+vw avazu.test.vw -t -i avazu.model.vw -p avazu.preds.txt --link logistic
+score - 0.4059982
+
+
+train - 
+~/code/vowpal_wabbit/utl/vw-hypersearch -L 1e-10 1e-6 vw avazu.train.vw -c --passes 300 -b 24 -f l1_model.vw --loss_function logistic --l1 %
+vw avazu.train.vw -c --passes 300 -b 24 -f avazu.model.vw --loss_function logistic --l1 1e-10
+vw avazu.train.vw -c --passes 8 -b 24 -f avazu.model.vw --holdout_off --loss_function logistic --l1 1e-10
+score - 0.4056106
+
+
+train (weighted examples) - 
+vw avazu.train.vw -c --passes 300 -b 24 -f avazu.model.vw --loss_function logistic
+vw avazu.train.vw -c --passes 13 -b 24 -f avazu.model.vw --holdout_off --loss_function logistic
+test -
+vw avazu.test.vw -t -i avazu.model.vw -p avazu.preds.txt --link logistic
+score - 0.4059982
